4999694952ebd4456fee276c2cada99de58d6da1
AvinashPotnuru1 <avinashpotnuru@techarion.com>
2024-03-25 13:29:50 +0530

    add outside click handler

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -18,6 +18,8 @@ import {
 } from "@/store/slice/tabsSlice";
 import { AiOutlineSearch, AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
+import OutsideClickHandlerWrapper from "../OutsideClickHandlerWrapper";
+
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
 //import components
@@ -111,24 +113,28 @@ const Navbar = () => {
               >
                 Categories
               </h1>
-              {dropDownCard && (
-                <div className="bg-white w-[500px] rounded-lg absolute top-12 p-4 hidden  md:grid grid-cols-3 gap-5 ">
-                  {moviesList?.genres?.map((item, idx) => (
-                    <Link
-                      className="hover:bg-slate-100"
-                      onClick={() => {
-                        // setOpen(false);
-                        dispatch(closeNav());
-                        setDropDownCard(false);
-                      }}
-                      href={`/category/${item?.id}`}
-                      key={idx}
-                    >
-                      {item?.name}
-                    </Link>
-                  ))}
-                </div>
-              )}
+              <OutsideClickHandlerWrapper
+                onOutsideClick={() => setDropDownCard(false)}
+              >
+                {dropDownCard && (
+                  <div className="bg-white w-[500px] rounded-lg absolute top-12 p-4 hidden  md:grid grid-cols-3 gap-5 ">
+                    {moviesList?.genres?.map((item, idx) => (
+                      <Link
+                        className="hover:bg-slate-100"
+                        onClick={() => {
+                          // setOpen(false);
+                          dispatch(closeNav());
+                          setDropDownCard(false);
+                        }}
+                        href={`/category/${item?.id}`}
+                        key={idx}
+                      >
+                        {item?.name}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </OutsideClickHandlerWrapper>
             </div>
 
             <div className="relative">
@@ -141,23 +147,27 @@ const Navbar = () => {
               >
                 Specials
               </h1>
-              {splDropDownCard && (
-                <div className="bg-white w-[190px]  rounded-lg absolute top-12 p-4 hidden  md:grid grid-cols-1 gap-3 ">
-                  {specials.map((item, idx) => (
-                    <Link
-                      className="hover:bg-slate-100"
-                      onClick={() => {
-                        dispatch(closeNav());
-                        setSplDropDownCard(false);
-                      }}
-                      href={`/special/${item?.page}`}
-                      key={idx}
-                    >
-                      {item?.name}
-                    </Link>
-                  ))}
-                </div>
-              )}
+              <OutsideClickHandlerWrapper
+                onOutsideClick={() => setSplDropDownCard(false)}
+              >
+                {splDropDownCard && (
+                  <div className="bg-white w-[190px]  rounded-lg absolute top-12 p-4 hidden  md:grid grid-cols-1 gap-3 ">
+                    {specials.map((item, idx) => (
+                      <Link
+                        className="hover:bg-slate-100"
+                        onClick={() => {
+                          dispatch(closeNav());
+                          setSplDropDownCard(false);
+                        }}
+                        href={`/special/${item?.page}`}
+                        key={idx}
+                      >
+                        {item?.name}
+                      </Link>
+                    ))}
+                  </div>
+                )}
+              </OutsideClickHandlerWrapper>
             </div>
 
             <Link href={"/favorite"}>
@@ -219,134 +229,136 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-      <div
-        className={` w-full   flex flex-col md:items-center bg-slate-600  md:pb-0 py-8 md:hidden absolute md:static   md:z-auto left-0  md:w-auto md:pl-0  transition-all duration-500 z-20 ease-in ${
-          open ? "top-16 " : "top-[-490px]"
-        }`}
-      >
-        <div className="   ">
-          <div className="flex  w-full py-4   ">
-            <div className="relative flex justify-between items-center px-3  w-full ">
-              <input
-                type="text"
-                onChange={(e) => {
-                  // setInput(e.target.value);
-                  setToggle(true);
-                  dispatch(searchInputHandler(e.target.value));
+      <OutsideClickHandlerWrapper onOutsideClick={() => dispatch(closeNav())}>
+        <div
+          className={` w-full   flex flex-col md:items-center bg-slate-600  md:pb-0 py-8 md:hidden absolute md:static   md:z-auto left-0  md:w-auto md:pl-0  transition-all duration-500 z-20 ease-in ${
+            open ? "top-16 " : "top-[-490px]"
+          }`}
+        >
+          <div className="   ">
+            <div className="flex  w-full py-4   ">
+              <div className="relative flex justify-between items-center px-3  w-full ">
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    // setInput(e.target.value);
+                    setToggle(true);
+                    dispatch(searchInputHandler(e.target.value));
+                  }}
+                  value={searchInput}
+                  className="py-2 w-[70%]  border border-gray-300  focus:outline-none focus:ring focus:border-blue-300"
+                  placeholder="Search Movie Name"
+                />
+                <Link className="" href={`/search-movies/${searchInput}`}>
+                  <button
+                    onClick={submitHandler}
+                    className=" text-white mr-5 border-2 font-semibold py-2 px-3 rounded-lg flex items-center"
+                  >
+                    <AiOutlineSearch size={20} />
+                  </button>
+                </Link>
+                {tabToggle && (
+                  <div
+                    ref={menuRef}
+                    className="bg-gray-800    absolute w-full h-[200px] overflow-hidden overflow-y-auto -bottom-48"
+                  >
+                    {searchData.length ? (
+                      searchData.map((e, idx) => (
+                        <SearchTabComponent
+                          tabHandler={() => setToggle()}
+                          clearInput={clearInput}
+                          key={idx}
+                          {...e}
+                        />
+                      ))
+                    ) : (
+                      <h1 className="text-red-500 text-center mt-[80px] my-auto">
+                        no results{" "}
+                      </h1>
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <Link href={"/favorite"}>
+              <h1
+                onClick={() => {
+                  dispatch(closeNav());
                 }}
-                value={searchInput}
-                className="py-2 w-[70%]  border border-gray-300  focus:outline-none focus:ring focus:border-blue-300"
-                placeholder="Search Movie Name"
-              />
-              <Link className="" href={`/search-movies/${searchInput}`}>
-                <button
-                  onClick={submitHandler}
-                  className=" text-white mr-5 border-2 font-semibold py-2 px-3 rounded-lg flex items-center"
-                >
-                  <AiOutlineSearch size={20} />
-                </button>
-              </Link>
-              {tabToggle && (
-                <div
-                  ref={menuRef}
-                  className="bg-gray-800    absolute w-full h-[200px] overflow-hidden overflow-y-auto -bottom-48"
-                >
-                  {searchData.length ? (
-                    searchData.map((e, idx) => (
-                      <SearchTabComponent
-                        tabHandler={() => setToggle()}
-                        clearInput={clearInput}
-                        key={idx}
-                        {...e}
-                      />
-                    ))
-                  ) : (
-                    <h1 className="text-red-500 text-center mt-[80px] my-auto">
-                      no results{" "}
-                    </h1>
-                  )}
-                </div>
-              )}
+                className={`ml-4 text-white font-bold mb-3 sm:mb-0 ${
+                  path === "/favorite" ? "" : ""
+                } `}
+              >
+                Favorite
+              </h1>
+            </Link>
+            <div className="flex justify-between items-center my-3">
+              <h1 className={`ml-4 text-white font-bold  `}>Categories</h1>
+
+              <div
+                onClick={() => setDropDown((prev) => !prev)}
+                className="mr-5 text-white"
+              >
+                {!dropDown ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}
+              </div>
             </div>
+            {dropDown && (
+              <div
+                className={`flex flex-col  overflow-y-auto    space-y-1 items-center  divide-y-2  ${
+                  dropDown && "h-[400px]"
+                }  `}
+              >
+                {moviesList?.genres?.map((item, idx) => (
+                  <Link
+                    className="w-full mx-auto px-5 py-2 text-white font-semibold"
+                    onClick={() => {
+                      dispatch(closeNav());
+                      setDropDown(false);
+                    }}
+                    href={`/category/${item?.id}`}
+                    key={idx}
+                  >
+                    {item?.name}
+                  </Link>
+                ))}
+              </div>
+            )}
+
+            <div className="flex justify-between items-center">
+              <h1 className={`ml-4 text-white font-bold  `}>Specials</h1>
+
+              <div
+                onClick={() => setSplDropDown((prev) => !prev)}
+                className="mr-5 text-white"
+              >
+                {!splDropDown ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}
+              </div>
+            </div>
+            {splDropDown && (
+              <div
+                className={`flex flex-col  overflow-y-auto    space-y-1 items-center  divide-y-2  ${
+                  splDropDown && "h-[300px]"
+                }  `}
+              >
+                {specials?.map((item, idx) => (
+                  <Link
+                    className="w-full mx-auto px-5 py-2 text-white font-semibold"
+                    onClick={() => {
+                      dispatch(closeNav());
+                      setSplDropDown(false);
+                    }}
+                    href={`/special/${item?.page}`}
+                    key={idx}
+                  >
+                    {item?.name}
+                  </Link>
+                ))}
+              </div>
+            )}
           </div>
-
-          <Link href={"/favorite"}>
-            <h1
-              onClick={() => {
-                dispatch(closeNav());
-              }}
-              className={`ml-4 text-white font-bold mb-3 sm:mb-0 ${
-                path === "/favorite" ? "" : ""
-              } `}
-            >
-              Favorite
-            </h1>
-          </Link>
-          <div className="flex justify-between items-center my-3">
-            <h1 className={`ml-4 text-white font-bold  `}>Categories</h1>
-
-            <div
-              onClick={() => setDropDown((prev) => !prev)}
-              className="mr-5 text-white"
-            >
-              {!dropDown ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}
-            </div>
-          </div>
-          {dropDown && (
-            <div
-              className={`flex flex-col  overflow-y-auto    space-y-1 items-center  divide-y-2  ${
-                dropDown && "h-[400px]"
-              }  `}
-            >
-              {moviesList?.genres?.map((item, idx) => (
-                <Link
-                  className="w-full mx-auto px-5 py-2 text-white font-semibold"
-                  onClick={() => {
-                    dispatch(closeNav());
-                    setDropDown(false);
-                  }}
-                  href={`/category/${item?.id}`}
-                  key={idx}
-                >
-                  {item?.name}
-                </Link>
-              ))}
-            </div>
-          )}
-
-          <div className="flex justify-between items-center">
-            <h1 className={`ml-4 text-white font-bold  `}>Specials</h1>
-
-            <div
-              onClick={() => setSplDropDown((prev) => !prev)}
-              className="mr-5 text-white"
-            >
-              {!splDropDown ? <IoMdArrowDropdown /> : <IoMdArrowDropup />}
-            </div>
-          </div>
-          {splDropDown && (
-            <div
-              className={`flex flex-col  overflow-y-auto    space-y-1 items-center  divide-y-2  ${
-                splDropDown && "h-[300px]"
-              }  `}
-            >
-              {specials?.map((item, idx) => (
-                <Link
-                  className="w-full mx-auto px-5 py-2 text-white font-semibold"
-                  onClick={() => {
-                    dispatch(closeNav());
-                    setSplDropDown(false);
-                  }}
-                  href={`/special/${item?.page}`}
-                  key={idx}
-                >
-                  {item?.name}
-                </Link>
-              ))}
-            </div>
-          )}
         </div>
-      </div>
+      </OutsideClickHandlerWrapper>
     </div>
   );
 };
